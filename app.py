9d2ccf716e4c924a3c130a74105370d0027c9537
privezentseved <privezentseved@gmail.com>
2026-07-19 12:36:27 +0300

    Correct version

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# Главная страница
+@app.route('/')
+def hello():
+    return '<h1>Привет, мир DevOps!</h1><p>Твой первый веб-сервер работает!</p>'
+
+# Страница статуса
+@app.route('/status')
+def status():
+    return 'OK'
+
+# Страница с логами
+@app.route('/logs')
+def show_logs():
+    log_path = '/home/prvzn/dev/my_first_service/logs/access.log'
+    try:
+        with open(log_path, 'r') as f:
+            content = f.read()
+        return f'<pre>Логи:\n{content}</pre>'
+    except FileNotFoundError:
+        return 'Файл логов пока не найден. Добавь туда что-нибудь!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
